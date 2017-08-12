c9825dfdd4c672a40a817385d9b667962b54716d
LBJ <LBJ>
2017-08-12 11:59:53 +0800

    20170812

--- app/src/main/java/tw/ming/app/helloworid/mystopwatch/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mystopwatch/MainActivity.java
@@ -2,12 +2,34 @@ package tw.ming.app.helloworid.mystopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean isRunning;
+    private Button left,right;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        left = (Button) findViewById(R.id.left);
+        rightt = (Button) findViewById(R.id.right);
+    }
+
+    //reset/stop
+    public void doLeft(View view){
+
+    }
+
+    //start/lap
+    public void doRight(View view){
+        isRunning = !isRunning;
+        if(isRunning){
+            right.setText("Stop");
+            left.setText("Lap");
+        }else{
+            right.setText("Start");
+            left.setText("reset");
+        }
     }
 }
